ba5462038e5e8f8c3e6f1e3cccfeb3d0b61812a8
chentongxiang <343069796@qq.cm>
2020-02-29 10:22:43 +0800

    广告页面完成，还未进行UI评审修改

--- dashboardAdv/js/common.js
+++ dashboardAdv/js/common.js
@@ -387,8 +387,8 @@ var options = {
                     fontWeight: 'normal',
                     color:this.titleColor
                 },
-                x:"3%",
-                y:"6%"
+                x:"6%",
+                y:"6.5%"
             },
             color: color,
             tooltip : {
@@ -400,7 +400,7 @@ var options = {
             grid: {
                 left: '5%',
                 right: '15%',
-                top:"25%",
+                top:"22%",
                 bottom: '8%',
                 containLabel: true
             },
@@ -459,8 +459,8 @@ var options = {
             ],
             series : barData,
             legend: {
-                right :"10%",
-                top : '10%',
+                right :"8%",
+                top : '7%',
                 // type: 'scroll',
                 orient: 'horizontal',
                 textStyle: {
@@ -490,7 +490,7 @@ var options = {
                     fontWeight: 'normal',
                     color:this.titleColor
                 },
-                x:"3%",
+                x:"6%",
                 y:"6%"
             },
             tooltip : {
@@ -500,9 +500,9 @@ var options = {
                 }
             },
             grid: {
-                left: '10%',
+                left: '4%',
                 right: '8%',
-                top:"25%",
+                top:"22%",
                 bottom: '8%',
                 containLabel: true
             },
@@ -520,6 +520,40 @@ var options = {
                             color:'#555656',  //坐标的字体颜色
                             fontSize:num?num/this.rate:14,
                         },
+                        interval:0,
+                        formatter : function(params){
+                            var newParamsName = "";// 最终拼接成的字符串
+                            var paramsNameNumber = params.length;// 实际标签的个数
+                            var provideNumber = 2;// 每行能显示的字的个数
+                            var rowNumber = Math.ceil(paramsNameNumber / provideNumber);// 换行的话，需要显示几行，向上取整
+                            /**
+                             * 判断标签的个数是否大于规定的个数， 如果大于，则进行换行处理 如果不大于，即等于或小于，就返回原标签
+                             */
+                            // 条件等同于rowNumber>1
+                            if (paramsNameNumber > provideNumber) {
+                                /** 循环每一行,p表示行 */
+                                for (var p = 0; p < rowNumber; p++) {
+                                    var tempStr = "";// 表示每一次截取的字符串
+                                    var start = p * provideNumber;// 开始截取的位置
+                                    var end = start + provideNumber;// 结束截取的位置
+                                    // 此处特殊处理最后一行的索引值
+                                    if (p == rowNumber - 1) {
+                                        // 最后一次不换行
+                                        tempStr = params.substring(start, paramsNameNumber);
+                                    } else {
+                                        // 每一次拼接字符串并换行
+                                        tempStr = params.substring(start, end) + "\n";
+                                    }
+                                    newParamsName += tempStr;// 最终拼成的字符串
+                                }
+
+                            } else {
+                                // 将旧标签的值赋给新标签
+                                newParamsName = params;
+                            }
+                            //将最终的字符串返回
+                            return newParamsName
+                        }
                     },
                     axisLine: {
                         lineStyle:{
@@ -781,12 +815,12 @@ var options = {
                     color:this.titleColor
                 },
                 x:"6%",
-                y:"7%"
+                y:"6%"
             },
             legend: {
                 orient: 'horizontal',
-                x:"60%",
-                y: '8%',
+                x:"72%",
+                y: '7%',
                 data:label,
                 textStyle: {
                     fontSize: num?num/this.rate:14,
